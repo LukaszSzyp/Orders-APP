7eccd59b34e57b04b0b6922ab8d3f8778200e141
Lukasz Szypulewski <szyp86@gmail.com>
2022-03-23 22:21:04 +0100

    make main nav bar and add styles

--- client/components/layout/MainNavigation.js
+++ client/components/layout/MainNavigation.js
@@ -6,35 +6,33 @@ const MainNavigation = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>HA SOLUTIONS</div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Strona domowa</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/assortment">
-              <a>Asortymenty</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/technologies">
-              <a>Technologie</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/contractors">
-              <a>Kontrachenci</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/offers">
-              <a>Oferty</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <ul>
+        <li>
+          <Link href="/" className={styles.link}>
+            <a>Strona domowa</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/assortment">
+            <a>Asortymenty</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/technologies">
+            <a>Technologie</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contractors">
+            <a>Kontrachenci</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/offers">
+            <a>Oferty</a>
+          </Link>
+        </li>
+      </ul>
     </header>
   );
 };
