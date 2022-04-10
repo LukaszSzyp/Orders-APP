69ea3a56037bc205cd39ac92043fd0d1091a8e43
Lukasz Szypulewski <szyp86@gmail.com>
2022-04-10 19:38:53 +0200

    add insert name to db in assortment

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "localhost:3306",
+  host: "localhost",
   user: "root",
   password: "root",
   database: "orderappdb",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/insert", (req, res) => {
+  const name = req.body.name;
+
+  const sqlInsert = "INSERT INTO orderappdb.assortment (name) VALUES (?);";
+  db.query(sqlInsert, [name], (err, result) => {
+    console.log(err);
+  });
 });
 
 app.listen(3001, () => {
