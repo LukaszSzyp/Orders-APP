c537d583d849911214249663bbf4537dbeaacb9c
Lukasz Szypulewski <szyp86@gmail.com>
2022-04-10 17:55:05 +0200

    test server connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 
-app.get("/", (req, res) => {
-  res.send("hello Lukas");
+const db = mysql.createPool({
+  host: "localhost:3306",
+  user: "root",
+  password: "root",
+  database: "orderappdb",
 });
 
 app.listen(3001, () => {
   console.log("running on port 3001");
 });
+
+/* app.get("/", (req, res) => {
+  const sqlInsert = "INSERT INTO orderappdb.assortment (name) VALUES ('test');";
+  db.query(sqlInsert, (err, result) => {
+    res.send("hello Lukas");
+  });
+}); */
